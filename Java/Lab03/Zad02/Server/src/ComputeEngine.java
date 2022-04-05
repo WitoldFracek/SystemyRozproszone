35e0221af5736d103e4805a0c52d5839cafe8484
Witold Frącek <witek.fracek@gmail.com>
2022-04-05 08:04:04 +0200

    server pi

--- Java/Lab03/Zad02/Server/src/ComputeEngine.java
+++ Java/Lab03/Zad02/Server/src/ComputeEngine.java
@@ -29,7 +29,7 @@ public class ComputeEngine implements Compute {
             String name = "Compute";
             Compute engine = new ComputeEngine();
             Compute stub = (Compute) UnicastRemoteObject.exportObject(engine, 0);
-            //Registry registry = LocateRegistry.getRegistry(args[0]);
+//            Registry registry = LocateRegistry.getRegistry();
             Registry registry = LocateRegistry.createRegistry(1099);
             registry.rebind(name, stub);
             System.out.println("ComputeEngine bound");
---
+++ Java/Lab03/Zad02/Server/src/ComputePi.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.math.BigDecimal;
+import java.io.File;
+
+public class ComputePi {
+
+    @SuppressWarnings("removal")
+    public static void main(String[] args) {
+//        File f = new File(".");
+//        File[] list = f.listFiles();
+//        for(File file: list) {
+//            if(file.isFile()) {
+//                System.out.println(file.getName());
+//            }
+//        }
+//        if(System.getSecurityManager() == null) {
+//            System.setSecurityManager(new SecurityManager());
+//        }
+
+        try {
+            String name = "Compute";
+            Registry registry = LocateRegistry.getRegistry(args[0]);
+            Compute comp = (Compute) registry.lookup(name);
+            Pi task = new Pi(Integer.parseInt(args[1]));
+            BigDecimal pi = comp.executeTask(task);
+            System.out.println(pi);
+        } catch(Exception e) {
+            System.err.println("ComputePi exception:");
+            e.printStackTrace();
+        }
+    }
+
+}
